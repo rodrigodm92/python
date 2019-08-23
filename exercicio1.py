9e8b74f620acd7275a459318c95f511cc8306337
Rodrigo Marros <rodrigo.do.marros@gmail.com>
2019-08-22 21:17:30 -0300

    First commit

---
+++ exercicio1.py
@@ -0,0 +1,5 @@
+idade=input ("Digite sua idade: ")
+if idade>=18:
+    print("maior de idade")
+else:
+        print("menor de idade")
---
+++ exercicio2.py
@@ -0,0 +1,12 @@
+nota1 = input("Digite a nota 1: ")
+nota2 = input("Digite a nota 2: ")
+nota3 = input("Digite a nota 3: ")
+media=(nota1+nota2+nota3)/3
+if media>=6:
+    media=str(media)
+    print("media: "+media)
+    print("Aprovado")
+else:
+    media=str(media)
+    print("media: "+media)
+    print("Reprovado")
